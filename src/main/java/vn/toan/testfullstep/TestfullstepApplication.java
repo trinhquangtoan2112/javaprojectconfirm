0e0c845bf2346d2f8e2e9f208ef3d6c746dbc01b
trinhquangtoan2112 <trinhtoanquang@gmail.com>
2025-04-14 14:39:00 +0700

    .

---
+++ src/main/java/vn/toan/testfullstep/TestfullstepApplication.java
@@ -0,0 +1,13 @@
+package vn.toan.testfullstep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestfullstepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestfullstepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/toan/testfullstep/TestfullstepApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.toan.testfullstep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestfullstepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
