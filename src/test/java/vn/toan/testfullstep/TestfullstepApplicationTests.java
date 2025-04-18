d0b6dad819325a81a751aa162651702cd2ce79dc
trinhquangtoan2112 <trinhtoanquang@gmail.com>
2025-04-18 14:34:28 +0700

    add afl

--- src/test/java/vn/toan/testfullstep/TestfullstepApplicationTests.java
+++ src/test/java/vn/toan/testfullstep/TestfullstepApplicationTests.java
@@ -1,13 +1,21 @@
 package vn.toan.testfullstep;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import vn.toan.testfullstep.controller.UserController;
 
 @SpringBootTest
 class TestfullstepApplicationTests {
 
+	@InjectMocks
+	private UserController user;
+
 	@Test
 	void contextLoads() {
+		Assertions.assertNotNull(user);
 	}
 
 }
